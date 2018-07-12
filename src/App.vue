0e23d4c51aaa6f76e07c39ba513b396ba14730e7
jonatanSousa <jonatan_sousa@hotmail.com>
2018-07-12 16:56:19 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <router-link class="nav-link" :to="'/'">home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="'/contact'">contact</router-link>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import PostExample from './components/Posts';
+import Contact from './components/Contact';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    PostExample
+  }
+}
+</script>
+
+<style>
+
+@import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+</style>
---
+++ src/components/Posts.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="container">
+
+        <table class="table mt-5">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">post</th>
+                <th scope="col">body</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr if v-for="post in posts" :key="post.id">
+                <th scope="row">{{post.id}}</th>
+                <td>{{post.title}}</td>
+                <td>{{post.body}}</td>
+            </tr>
+
+            </tbody>
+        </table>
+        <button class="btn btn-primary">click</button>
+
+    </div>
+</template>
+
+
+<script>
+import axios from "axios";
+
+    export default {
+        name: 'postExample',
+        data() {
+            return {
+                posts: [
+
+                ]
+            }
+        },
+
+        created(){
+            axios.get('https://jsonplaceholder.typicode.com/posts').then(post =>{
+
+                this.posts = post.data;
+            })
+        }
+    }
+
+</script>
+
+
+<style>
+
+
+</style>
